934b5fb46e54d42afacc2a673f1d1299416b4551
NoaYuuki <noadevillers@live.fr>
2021-07-02 20:04:25 +0200

    first commit

---
+++ src/Entity/Post.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use App\Controller\PostPublishController;
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Valid;
+
+/**
+ * @ORM\Entity(repositoryClass=PostRepository::class)
+ */
+#[ApiResource(
+    normalizationContext: [
+        'groups' => ['read:collection'],
+        'openapi_definition_name' => 'Definition'
+    ],
+    denormalizationContext: ['groups' => ['write:Post']],
+    paginationItemsPerPage: 2,
+    paginationMaximumItemsPerPage: 2,
+    paginationClientItemsPerPage: true,
+    collectionOperations: [
+        'get', 
+        'post' => [
+            'validation_groups' => [Post::class, 'validationGroups']
+        ],
+        'count' => [
+            'method' => 'GET',
+            'path' => '/posts/count',
+            'controller' => PostCountController::class,
+            'read' => false,
+            'pagination_enabled' => false,
+            'filters' => [],
+            'openapi_context' => [
+                'summary' => 'Récupère le nombre total d\'articles existants',
+                'parameters' => [
+                    [
+                        'in' => 'query',
+                        'name' => 'online',
+                        'schema' => [
+                            'type' => 'integer',
+                            'maximum' => 1,
+                            'minimum' => 0
+                        ],
+                        'description' => 'Filtre les articles en ligne'
+                    ]
+                ],
+            ]
+        ]
+    ],
+    itemOperations: [
+        'put',
+        'delete',
+        'get' => [
+            'normalization_context' => [
+                'groups' => ['read:collection', 'read:item', 'read:Post'],
+                'openapi_definition_name' => 'Details'
+                ]
+        ],
+        'publish' => [
+            'method' => 'POST',
+            'path' => '/posts/{id}/publish',
+            'controller' => PostPublishController::class,
+            'openapi_context' => [
+                'summary' => 'Permet de publier un article en ligne',
+                'requestBody' => [
+                    'content' => [
+                        'application/json' => [
+                            'schema' => []
+                        ]
+                    ]
+                ]
+            ]
+        ]
+    ]
+),
+   ApiFilter(SearchFilter::class, properties: ['id' => 'exact', 'title' => 'partial'])     
+]
+class Post
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    #[Groups(['read:collection'])]
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    #[
+        Groups(['read:collection', 'write:Post']),
+        Length(min: 5, groups: ['create:Post'])
+    ]
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    #[Groups(['read:collection', 'write:Post'])]
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    #[Groups(['read:item', 'write:Post'])]
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    #[Groups(['read:item'])]
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $updatedAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="posts")
+     */
+    #[
+        Groups(['read:item', 'read:collection', 'write:Post']),
+        Valid()    
+    ]
+    private $category;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default": "0"})
+     */
+    #[
+        Groups(['read:collection']),
+        ApiProperty(openapiContext: ['type' => 'boolean', 'description' => 'Statut de publication'])
+    ]
+    private $online = false;
+
+    // public static function validationGroups(self $post) {
+    //     return ['create:Post'];
+    // }
+
+    public function __construct() {
+        $this->createdAt = new \DateTime();
+        $this->updatedAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getOnline(): ?bool
+    {
+        return $this->online;
+    }
+
+    public function setOnline(bool $online): self
+    {
+        $this->online = $online;
+
+        return $this;
+    }
+}
